e72ac8e48c3b489b4a74e89cc59e63a31054976f
Yuliya790 <tushina.yuliya@mail.ru>
2026-04-15 13:41:01 +0000

    Add test cat image and fix settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -96,7 +96,7 @@ STATIC_ROOT = '/var/www/kittygram/static/'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/kittygram/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
