4b18b0c371cebd20dce7a1452a4b5c1c021f1944
Yuliya790 <tushina.yuliya@mail.ru>
2026-04-15 14:24:11 +0000

    Fix Djoser URLs and update credentials to testuser

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/', include('djoser.urls')),  # Работа с пользователями
-    path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
+    path('api/auth/', include('djoser.urls.authtoken')),  # Работа с токенами
 ]
 
 if settings.DEBUG:
